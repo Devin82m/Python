3cf240a05b70286649cd29267e3f625a65fa80e4
devinst.clair@gmail.com <dstclair@dstclair.local>
2017-11-27 16:36:42 -0600

    2017-11-27+

--- CSVcleaner.py
+++ CSVcleaner.py
@@ -1,9 +1,13 @@
+# Example of use: python CSVcleaner.py ~/Downloads/source_file ~/Downloads/destination_file 1 14
+# 1 and 14 being the range of rows you wish to remove, it will remove rows 1-13 (I'm not sure why it won't grab the last line, but take that in account)
+
+
 import csv
 import sys
 
 sourcef = sys.argv[1]  # Source file path/name
 destf = sys.argv[2]  # Destination file path/name
-linestart = int(sys.argv[3])  # Starting row to delete
+linestart = int(sys.argv[3])  # Starting row to delete, remember 0 is the first row and typically is the header
 lineend = int(sys.argv[4])  # Ending row to delete, but add one
 
 with open (sourcef, 'rb') as file1, open (destf, 'wb') as file2:
