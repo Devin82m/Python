4de890f9eda3c03312f9c9e5de32a163673f62dd
devinst.clair@gmail.com <dstclair@dstclair.local>
2017-11-28 15:20:44 -0600

    2017-11-28

--- CSVcleaner.py
+++ CSVcleaner.py
@@ -7,7 +7,7 @@ from itertools import islice
 sourcef = sys.argv[1]  # Source file path/name e.g. ~/Documents/something.csv
 destf = sys.argv[2]  # Destination file path/name e.g. ~/Documents/something.csvCLEAN
 linestart = int(sys.argv[3])  # Starting row to delete, remember 0 is the first row and typically is the header
-lineend = int(sys.argv[4])  # Ending row to delete, but add one
+lineend = int(sys.argv[4])  # Ending row to delete
 
 with open (sourcef, 'rb') as file1, open (destf, 'wb') as file2:
     reader = csv.reader(file1)
